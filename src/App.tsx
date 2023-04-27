f188a238465cc549799e74293bad2ae7f5e668e0
edikk <edikkopcha6@gmail.com>
2023-04-27 13:34:12 +0300

    Added functionality delete current element

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,67 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {FormEventHandler, MouseEventHandler, useState} from 'react';
+import Task from "./tasks";
+
+
+
+function Tasks (props:Task){
+
+    return(
+        <>
+            <div>
+                <h1>{props.task.title}</h1>
+                <p>{props.task.task}</p>
+                <button onClick={()=>props.button(props.index)}>Delete</button>
+            </div>
+        </>
+    )
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+    const [state,setState]= useState<{title:string,task:string}>({title:'',task:''});
+    const [arr,setArr]= useState<{title:string,task:string}[]>([]);
+
+    const onChangeTitle: React.ChangeEventHandler<HTMLInputElement> = (e) =>{
+        setState({...state,title:e.target.value})
+    }
+
+    const onChangeTask: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+        setState({...state, task: e.target.value})
+    }
+
+    const onClick:MouseEventHandler<HTMLButtonElement> =()=> {
+         setArr([...arr,state]);
+         setState({title:'',task:''});
+     }
+
+     const submit:FormEventHandler<HTMLFormElement>=(e)=>{
+        e.preventDefault();
+     }
+
+     const Delete = (i:number)=> {
+        const oldArr = [...arr];
+        oldArr.splice(i,1)
+        setArr(oldArr)
+    }
+
+  return(
+      <>
+          <form onSubmit={submit}>
+              <label>To-do tasks</label>
+              <input type='text' onChange={onChangeTitle} value={state.title} />
+              <input type='text' onChange={onChangeTask} value={state.task} />
+              <button onClick={onClick}>Save</button>
+          </form>
+          <div>
+              {arr.map((e,i)=>
+                  <div key={i}>
+                    <Tasks task={e} button={Delete} index={i}/>
+                  </div>
+              )}
+          </div>
+      </>
+  )
+
 }
 
 export default App;
---
+++ src/tasks.ts
@@ -0,0 +1,8 @@
+import {MouseEventHandler} from "react";
+
+interface Task{
+    task:{title:string,task:string};
+    button:any;
+    index:number;
+}
+export default Task
